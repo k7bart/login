9263d7b45a5b445e8efeceb7ed199a926556401b
kate <k7bart@gmail.com>
2024-01-08 18:23:25 +0200

    update

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -2,19 +2,64 @@ import React from "react";
 import "./LoginForm.css";
 import { FaUser, FaLock } from "react-icons/fa";
 
-// import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const LoginForm = () => {
+    // розібратись для чого
+    const userRef = useRef();
+    const errorRef = useRef();
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrorMessage("");
+    }, [user, password]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div className="wrapper">
-            <form action="">
-                <h1>Login</h1>
+        <section className="wrapper">
+            <p
+                ref={errorRef}
+                // додати класи
+                className={errorMessage ? "errormessage" : "offscreen"}
+                aria-live="assertive"
+            >
+                {errorMessage}
+            </p>
+
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" required />
+                    <input
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        placeholder="Username"
+                        onChange={(e) => setUser(e.target.value)}
+                        value={user}
+                        required
+                    />
                     <FaUser className="icon" />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required />
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        required
+                    />
                     <FaLock className="icon" />
                 </div>
                 <div className="remember-forgot">
@@ -25,13 +70,14 @@ const LoginForm = () => {
                     <a href="#">Forgot password?</a>
                 </div>
                 <button type="sumbit">Login</button>
-                <div className="register-link">
-                    <p>
-                        Don't have an account? <a href="#">Register</a>
-                    </p>
-                </div>
             </form>
-        </div>
+            <div className="register-link">
+                {/* put router link here */}
+                <p>
+                    Don't have an account? <a href="#">Register</a>
+                </p>
+            </div>
+        </section>
     );
 };
 
